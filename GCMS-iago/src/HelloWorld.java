46bf3b0badb934d01f8c63a01255ad2871245b98
iagopedro <iagopedroltd19@gmail.com>
2023-05-30 18:50:32 -0300

    quarto commit

--- GCMS-iago/src/HelloWorld.java
+++ GCMS-iago/src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World!!!!!!!!!1111!!!!1111!!!!1111");
+		System.out.println("Hello World!!!!!!!!!2222!!!!2222!!!!2222");
 	}
 
 }
